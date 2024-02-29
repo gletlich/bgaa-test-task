b6edf04349340177f7d9dcf50f1311e86b99c275
gletlich <gletlich@gmail.com>
2024-02-29 09:45:06 +0300

    feat: limit width of app

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -2,8 +2,14 @@ import Groups from "@/components/groups/groups.component";
 
 import "./app.styles.scss";
 
+import classes from "./app.module.scss";
+
 const App = () => {
-  return <Groups />;
+  return (
+    <div className={classes.container}>
+      <Groups />
+    </div>
+  );
 };
 
 export default App;
