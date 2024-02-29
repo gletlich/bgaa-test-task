0d3102b9892b62da4ea418ceb46cf9efb3fb4e73
gletlich <gletlich@gmail.com>
2024-02-29 14:36:32 +0300

    feat: add button to assign teacher

--- src/components/table-row/table-row.component.tsx
+++ src/components/table-row/table-row.component.tsx
@@ -5,6 +5,8 @@ import { useAppSelector } from "@/store/store";
 import { getGroupData } from "@/features/groups/groups.slice";
 import { getTeachers } from "@/features/teachers/teachers.slice";
 
+import SortIcon from "../icons/sort.icon";
+
 import type { TableRowProps } from "./table-row.types";
 
 import classes from "./table-row.module.scss";
@@ -24,28 +26,45 @@ const TableRow = (props: TableRowProps) => {
 
   const isOnePodgroup = group.podgroups.length === 1;
 
+  const select = (
+    <Select
+      options={options}
+      defaultValue={options[0]}
+      menuPortalTarget={document.body}
+      menuPosition={"fixed"}
+      isDisabled={hours === "0"}
+    />
+  );
+
   return (
     <tr className={classes.row}>
       <td>{activity}</td>
       <td>{hours}</td>
       <td>
-        <Select
-          options={options}
-          defaultValue={options[0]}
-          menuPortalTarget={document.body}
-          menuPosition={"fixed"}
-          isDisabled={hours === "0"}
-        />
+        {activity === "Лекции" ? (
+          <div className={classes.lections}>
+            {select}
+            <button className={classes.sortBtn}>
+              <SortIcon />
+            </button>
+          </div>
+        ) : (
+          select
+        )}
       </td>
+
       {!isOnePodgroup && (
         <td>
-          <Select
-            options={options}
-            defaultValue={options[0]}
-            menuPortalTarget={document.body}
-            menuPosition={"fixed"}
-            isDisabled={hours === "0"}
-          />
+          {activity === "Лекции" ? (
+            <div className={classes.lections}>
+              {select}
+              <button className={classes.sortBtn}>
+                <SortIcon />
+              </button>
+            </div>
+          ) : (
+            select
+          )}
         </td>
       )}
     </tr>
