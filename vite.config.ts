fd84e0500fb3c8a783e048056f784aa4538b63de
gletlich <gletlich@gmail.com>
2024-02-29 21:26:51 +0300

    feat: add base to vite config for gh pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = import.meta.dirname;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
