c7d3014a8f41a29408fbc68e14ad0736d6307cff
gletlich <gletlich@gmail.com>
2024-02-29 21:35:15 +0300

    fix: change base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = import.meta.dirname;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/bgaa-test-task/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
